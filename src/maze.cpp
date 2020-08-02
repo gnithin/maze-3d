c98c461a38789253644c95ef47ef21af60f50d27
Nithin <nithin.linkin@gmail.com>
2020-08-02 15:01:36 -0400

    [setup] Add a maze point and calculate the floor size

--- src/maze.cpp
+++ src/maze.cpp
@@ -2,6 +2,11 @@
 
 #define OBJECTS 4
 
+std::vector<std::vector<MazePoint>> mazeMatrix{
+    {MazePoint(0, 1, 0, 1), MazePoint(0, 0, 0, 0)},
+    {MazePoint(0, 0, 0, 0), MazePoint(1, 0, 1, 0)},
+};
+
 Maze::Maze(int w, int h) : screenWidth(w), screenHeight(h)
 {
     // Setup our objects
@@ -32,23 +37,34 @@ Maze::~Maze()
 
 void Maze::update()
 {
-    objects[0]->getTransform().loadIdentity();
-    objects[0]->getTransform().translate(0.0f, 0.0f, 0.0f);
-    objects[0]->update(screenWidth, screenHeight);
+    int matrixSize = mazeMatrix.size();
+    float singleSize = 2.0f;
 
-    objects[1]->getTransform().loadIdentity();
-    objects[1]->getTransform().translate(1.0f, 0.0f, 0.0f);
-    objects[1]->getTransform().rotate(glm::radians(90.0f), 0.0f, 1.0f, 0.0f);
-    objects[1]->update(screenWidth, screenHeight);
+    // // Walls
+    // objects[0]->getTransform().loadIdentity();
+    // objects[0]->getTransform().translate(0.0f, 0.0f, 0.0f);
+    // objects[0]->update(screenWidth, screenHeight);
 
-    objects[2]->getTransform().loadIdentity();
-    objects[2]->getTransform().translate(-1.0f, 0.0f, 0.0f);
-    objects[2]->getTransform().rotate(glm::radians(90.0f), 0.0f, 1.0f, 0.0f);
-    objects[2]->update(screenWidth, screenHeight);
+    // objects[1]->getTransform().loadIdentity();
+    // objects[1]->getTransform().translate(1.0f, 0.0f, 0.0f);
+    // objects[1]->getTransform().rotate(glm::radians(90.0f), 0.0f, 1.0f, 0.0f);
+    // objects[1]->update(screenWidth, screenHeight);
 
-    objects[3]->getTransform().loadIdentity();
+    // objects[2]->getTransform().loadIdentity();
+    // objects[2]->getTransform().translate(-1.0f, 0.0f, 0.0f);
+    // objects[2]->getTransform().rotate(glm::radians(90.0f), 0.0f, 1.0f, 0.0f);
+    // objects[2]->update(screenWidth, screenHeight);
+
+    // Floor
+    float scaleFactor = (matrixSize - 1) * singleSize;
+    std::cout << "Scale factor - " << scaleFactor << std::endl;
+
+    // Scale the floor according to the size of the matrix
+    objects[3]
+        ->getTransform()
+        .loadIdentity();
     objects[3]->getTransform().translate(0.0f, -1.0f, 0.0f);
-    objects[3]->getTransform().scale(4.0f, 4.0f, 4.0f);
+    objects[3]->getTransform().scale(scaleFactor, scaleFactor, scaleFactor);
     objects[3]->getTransform().rotate(glm::radians(90.0f), 1.0f, 0.0f, 0.0f);
     objects[3]->update(screenWidth, screenHeight);
 }
